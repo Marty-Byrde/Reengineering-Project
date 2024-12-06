0a674f2f73f3bebf2ddd153882ea8340e91865b7
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-06 13:30:59 +0100

    ref: moved kotlin integration test to androidTest directory
    Moved an exemplary integration test to the dedicated androidTest directory so that it has access to the Android dependencies.

--- src/androidTest/kotlin/editor/KotlinIntegrationTest.kt
+++ src/androidTest/kotlin/editor/KotlinIntegrationTest.kt
@@ -1,25 +1,29 @@
-package org.billthefarmer.editor
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.billthefarmer.editor.Editor
+import org.billthefarmer.editor.R
 import org.junit.Rule
 import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class KotlinIntTest {
+class KotlinIntegrationTest {
 
-    @get:Rule
-    val activityRule = ActivityScenarioRule(Editor::class.java)
+//    @get:Rule
+//    val activityRule = ActivityScenarioRule(Editor::class.java)
 
 
     @Test
     fun exampleIntegration(){
         val filePath = ""
 
-        onView(withId(R.id.open)).perform(click())
+
+        val textView = withId(R.id.text)
+        System.out.println(textView.toString())
+        onView(textView).perform(click())
     }
 }
