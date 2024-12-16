0ba1aa719afee24e6e06cfa4aac7ff9f6d50b720
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-16 15:53:52 +0100

    feat: created Preferences enum
    This enum holds the different preference options used within the Editor.

---
+++ src/main/java/org/billthefarmer/editor/preferences/Preferences.java
@@ -0,0 +1,5 @@
+package org.billthefarmer.editor.preferences;
+
+public enum Preferences {
+    autoSaveFeature, isReadOnly, isLast, isContentWrapped, isSuggestEnabled, isHighlightEnabled, Theme, FontSize, FontType, pathSet,
+}
