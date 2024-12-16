a2b89da92b0a8ab77c5053acf6a3d5c3dfee077c
Marty Byrde <45905689+Marty-Byrde@users.noreply.github.com>
2024-12-16 11:14:11 +0100

    feat: added exemplary integration test
    This integration test is meant to simulate the create and save file feature. However, as of right now the permissions to access the files on the file prevent the test from running as expected.

--- src/androidTest/kotlin/editor/KotlinIntegrationTest.kt
+++ src/androidTest/kotlin/editor/KotlinIntegrationTest.kt
@@ -1,11 +1,17 @@
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isClickable
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isNotClickable
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.billthefarmer.editor.Editor
 import org.billthefarmer.editor.R
+import org.hamcrest.core.IsNot.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,6 +21,24 @@ class KotlinIntegrationTest {
 
     @get:Rule
     val activityRule = ActivityScenarioRule(Editor::class.java)
+
+    @Test
+    fun testCreateAndSaveFile_WithContent(){
+
+        // Verify pre-conditions edit visible, view invisible
+        onView(withId(R.id.view)).check(matches((isNotClickable())))
+        onView(withId(R.id.edit)).check(matches((isClickable())))
+
+
+        // Click edit icon to disable read-only mode
+        onView(withId(R.id.edit)).perform(click())
+
+        onView((withId(R.id.edit))).check(matches(withText("")))
+
+        // Edit button should now be invisible
+        onView(withId(R.id.edit)).check(matches(isNotClickable()))
+        onView(withId(R.id.view)).check(matches(isClickable()))
+    }
 
 
     @Test
